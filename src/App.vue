effb0356b00d05e2c8c99aff42587aece1e8fde7
you <you@exemplo.com>
2019-06-24 20:37:18 -0300

    testegit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+  <h1>tarefas</h1> 
+  <taskgrid :tasks="tasks"/>
+    
+  </div>
+</template>
+
+<script>
+import taskgrid from './components/taskgrid.vue'
+
+export default {
+  components: {taskgrid},
+  name: 'app',
+  data(){
+    return{
+      tasks:[
+        {name:'comprar remedio',pending:false},
+        {name:'passear com o cachorro',pending:true}
+        ]
+
+    }
+  }
+}
+</script>
+
+<style>
+body{
+background: #F7971E;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #FFD200, #F7971E);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #FFD200, #F7971E); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+#app{
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1{
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 5rem;
+}
+</style>
---
+++ src/components/taskgrid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="task-grid">
+        <task v-for="task in tasks" :key="task.name" :task="task"></task>
+    </div>
+    
+</template>
+
+<script>
+import task from './task.vue'
+export default {
+    components:{task},
+   props:{
+       tasks:{type:Array, required:true}
+   }
+
+}
+</script>
+
+<style>
+
+</style>
